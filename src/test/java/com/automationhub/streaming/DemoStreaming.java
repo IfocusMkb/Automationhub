fd65ca7cb5c917c6101ab1e9255443f718bc08ca
iFocus <mkumar.info34@gmail.com>
2024-12-30 20:41:25 +0530

    2nd commit

---
+++ src/test/java/com/automationhub/streaming/DemoStreaming.java
@@ -0,0 +1,10 @@
+package com.automationhub.streaming;
+
+public class DemoStreaming {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Updated file");
+	}
+
+}
